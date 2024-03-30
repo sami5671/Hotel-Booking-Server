0c5dfa6d0283b99d8962efee8b3d589b10ac3048
sami5671 <samialam5671@gmail.com>
2024-03-30 20:29:47 +0600

    role based daashboard api

--- index.js
+++ index.js
@@ -95,7 +95,20 @@ async function run() {
       const options = { upsert: true };
       const isExist = await usersCollection.findOne(query);
       console.log("User found?----->", isExist);
-      if (isExist) return res.send(isExist);
+
+      if (isExist) {
+        if (user?.status === "Requested") {
+          const result = await usersCollection.updateOne(
+            query,
+            { $set: user },
+            options
+          );
+          return res.send(result);
+        } else {
+          return res.send(isExist);
+        }
+      }
+
       const result = await usersCollection.updateOne(
         query,
         {
@@ -185,6 +198,38 @@ async function run() {
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
     });
+    // Get all bookings for host
+    app.get("/bookings/host", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      if (!email) return res.send([]);
+      const query = { host: email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get all users
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    // update user role
+    app.put("/users/update/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
+    // become a host
+
     // ===========================================================================
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
